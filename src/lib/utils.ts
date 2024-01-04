62d1ba0d2d49b50527debea20b7c00ae968fc28e
tejasvi raj <tejasviraj8686@gmail.com>
2024-01-04 11:18:15 +0530

    added shopping cart

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,28 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function formatPrice(
+  price: number | string,
+  options: {
+    currency?: 'USD' | 'EUR' | 'GBP' | 'BDT'| "INR";
+    notation?: Intl.NumberFormatOptions['notation'];
+  } = {}
+) {
+  const { currency = 'INR', notation = 'compact' } = options;
+
+  const numericPrice =
+    typeof price === 'string' ? parseFloat(price) : price;
+
+  const locale = 'en-IN'; 
+
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency,
+    notation,
+    maximumFractionDigits: 2,
+  }).format(numericPrice);
+}
+
+
