1cb0cc8d4afd8f6b310dbadbce78c95cdef55c83
tejasvi raj <tejasviraj8686@gmail.com>
2024-01-08 23:22:47 +0530

    bug-fix

--- src/components/providers.ts
+++ src/components/providers.ts
@@ -1,10 +1,10 @@
-"use clients"
-import { useState } from "react"
+"use client"
+import { PropsWithChildren, useState } from "react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { trpc } from '@/trpc/client'
 import { httpBatchLink } from "@trpc/client";
 
-const providers = () => {
+const providers = ({ children }: PropsWithChildren) => {
 
     const [queryClient] = useState(() => new QueryClient())
     const [trpcClient] = useState(() => trpc.createClient({
@@ -21,9 +21,7 @@ const providers = () => {
         ],
     })
     )
-   return (
     
-   )
 }
 
 
